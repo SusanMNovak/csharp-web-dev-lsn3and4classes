d7e9758442e70e3e8c4e5db4367e29c4c6eb9af9
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-07-27 21:39:33 -0500

    Completed exercise!!!

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public List<Student> Roster { get; set; }
+        public double NumCredits { get; set; }
+
+
+        public Course()
+        {
+            this.Roster = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            if (this.Roster == null)
+            {
+                this.Roster = new List<Student>();
+            }
+
+            this.Roster.Add(student);
+        }
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,24 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student me = new Student();
+
+            me.Name = "Susan";
+            me.StudentId = 101;
+            me.NumberOfCredits = 1;
+            me.Gpa = 4.0;
+
+            Console.WriteLine($"{me.Name} ({me.StudentId})");
+            Console.WriteLine($"Credits: {me.NumberOfCredits}, GPA: {me.Gpa}");
+
+            // Generate a new Course
+            Course math = new Course();
+            math.Id = 202;
+            math.Name = "Discrete Structures";
+            math.Length = 17;
+            math.NumCredits = 4.0;
+            math.Roster = new List<Student>();
+
         }
     }
 }
